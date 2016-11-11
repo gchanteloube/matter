cd8e2d2e0bd721854fcbeed68de1efac075767b9
gchanteloube <admin@example.com>
2016-11-11 11:38:11 +0100

    Model

--- kernel/dependency/Utils.php
+++ kernel/dependency/Utils.php
@@ -31,10 +31,33 @@ class Utils {
         }
     }
 
-    public static function getEnvironment () {
+    public static function getEnvironment() {
         $environment_ini = parse_ini_file("../conf/environment.ini", true);
         $environment = $environment_ini['current_environment']['environment'];
         return $environment_ini[$environment];
+    }
+
+    public static function getDatabase($dbName = null) {
+        $database_ini = parse_ini_file("../conf/database.ini", true);
+        if (Utils::valid($dbName)) {
+            if (array_key_exists($dbName, $database_ini)) {
+                return $database_ini[$dbName];
+            } else {
+                throw new \Exception('This database (' . $dbName . ') doesn\'t exist. Check your "database.ini" file please.');
+            }
+        } else {
+            $environment = Utils::getEnvironment();
+            if (array_key_exists('db', $environment)) {
+                $dbName = $environment['db'];
+                if (array_key_exists($dbName, $database_ini)) {
+                    return $database_ini[$dbName];
+                } else {
+                    throw new \Exception('This database (' . $dbName . ') doesn\'t exist. Check your "database.ini" file please.');
+                }
+            } else {
+                throw new \Exception('You have to define "db" entity in your "environment.ini", linked to "database.ini" file please.');
+            }
+        }
     }
 }
 
--- kernel/interfaces/IModel.php
+++ kernel/interfaces/IModel.php
@@ -10,26 +10,8 @@ namespace Matter;
 
 abstract class IModel {
     protected function db($dbName = null) {
-        $database_ini = parse_ini_file("../conf/database.ini", true);
-        if (Utils::valid($dbName)) {
-            if (array_key_exists($dbName, $database_ini)) {
-                return new Db($database_ini[$dbName]);
-            } else {
-                throw new \Exception('This database (' . $dbName . ') doesn\'t exist. Check your "database.ini" file please.');
-            }
-        } else {
-            $environment = Utils::getEnvironment();
-            if (array_key_exists('db', $environment)) {
-                $dbName = $environment['db'];
-                if (array_key_exists($dbName, $database_ini)) {
-                    return new Db($database_ini[$dbName]);
-                } else {
-                    throw new \Exception('This database (' . $dbName . ') doesn\'t exist. Check your "database.ini" file please.');
-                }
-            } else {
-                throw new \Exception('You have to define "db" entity in your "environment.ini", linked to "database.ini" file please.');
-            }
-        }
+        $dbInfo = Utils::getDatabase($dbName);
+        return new Db($dbInfo);
     }
 }
 
